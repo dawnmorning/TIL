ef370c0744b6dbfb731d2f07a4cd719a9246a32c
dawnmorning <notify9637@naver.com>
2022-07-24 22:32:03 +0900

    0724 TIL

---
+++ 0720/TIL.py
@@ -0,0 +1,36 @@
+# num = int(input('숫자입력 : '))
+# if num % 2 == 1 :
+#     print('홀수')
+# else:
+#     print('짝수')
+
+# dust = int(input('오늘의 미세먼지 : ' ))
+# if dust > 150 :
+#     print('매우 나쁨')
+#     if dust > 300:
+#         print('실외 활등을 자제하세요.')
+# elif dust > 80:
+#     print('나쁨')
+# elif dust > 30:
+#     print('보통')
+# elseif dust >= 30:
+#     print('좋음')
+# else:
+#     print('값이 잘못 되었습니다.')
+
+# 삼항 연산자
+# '홀수' if num % 2 else 짝수
+# num = -5
+# if num >= 0:
+#     value = num
+# else:
+#    value = 0
+# print(num)
+# a = 0
+# while a < 5:
+#     print(a)
+#     a += 1
+# print('끝')
+# chars = input()
+# for i in chars:
+#     print(i)
