440755768c878c3b93de94d67e3d723d36e4f73d
dawnmorning <notify9637@naver.com>
2022-09-29 01:47:24 +0900

    modify

--- MST1.py
+++ MST1.py
@@ -1,9 +1,9 @@
 v,e = map(int, input().split())
-adjM = [[0] * v+1 for _ in range(v+1)]
+adjM = [[0] * (v+1) for _ in range(v+1)]
 adjL = [[] for _ in range(v+1)]
 for _ in range(e):
     u,v,w = map(int, input().split())
     adjM[u][v] = w
-    adjM[v][i] = w
+    adjM[v][u] = w
     adjL[u].append((u,w))
     adjL[u].append((v,u))
