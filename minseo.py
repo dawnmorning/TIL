e17c174c1c8f00519c8c7e08348a80a1c701866c
dawnmorning <notify9637@naver.com>
2022-11-04 20:56:43 +0900

    modify

--- minseo.py
+++ minseo.py
@@ -4,6 +4,7 @@ if int(num1) > 0 and int(num2) > 0:
 
 
 # 2.
+month = input('월에 해당하는 정수를 입력하시오:' )
 month = int(month)
 if 3 <= month <=5:
     print(month, '월은 봄입니다.')
@@ -16,32 +17,32 @@ elif month == 12 or 1 <= month <=2 :
 
 # 3.
 
-a = float(input("첫번째 숫자를 입력하시오: "))
-b = float(input("두번째 숫자를 입력하시오: "))
-y = 0
-if a > 0 :
-    y = y + 5
-else:
-    print("안녕")
-    print(str(-1.0 * a))
-print(y)
+# a = float(input("첫번째 숫자를 입력하시오: "))
+# b = float(input("두번째 숫자를 입력하시오: "))
+# y = 0
+# if a > 0 :
+#     y = y + 5
+# else:
+#     print("안녕")
+#     print(str(-1.0 * a))
+# print(y)
 
 # 4.
 
-for i in range(-5, 5):
-    x = int(input())
-    if x == 0 :
-        print('Zero')
-    elif x % 3 == 0:
-        print(x, "는 3의 배수입니다.")
-    else:
-        print(x, '는 3의 배수가 아닙니다.')
+# for i in range(-5, 5):
+#     x = int(input())
+#     if x == 0 :
+#         print('Zero')
+#     elif x % 3 == 0:
+#         print(x, "는 3의 배수입니다.")
+#     else:
+#         print(x, '는 3의 배수가 아닙니다.')
 
 # 5.
-s = 1
-for i in range(3,100,2):
-    s += i
-print(s)
+# s = 1
+# for i in range(3,100,2):
+#     s += i
+# print(s)
 
 # 6.
 for i in range(5,0,-1):
