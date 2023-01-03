a1febbc989303960358ad89c53e1d427a60e8abc
김종혁 <notify9637@naver.com>
2023-01-03 23:55:32 +0900

    edit

--- React/노마드 코더/movie_project/src/routes/Detail.js
+++ React/노마드 코더/movie_project/src/routes/Detail.js
@@ -1,18 +1,35 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState([true]);
+  const [details, setDetails] = useState([]);
   //   console.log(id);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    console.log(json.data.movie.genres);
+    setDetails(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      <div>
+        <h2>{details.title_long}</h2>
+      </div>
+      <div>
+        <img src={details.medium_cover_image} alt="" />
+      </div>
+      <ul>
+        {loading ? "Loading..." : details.genres.map((g) => <li>{g}</li>)}
+      </ul>
+      <div>{details.description_full}</div>
+    </div>
+  );
 }
 export default Detail;
--- React/노마드 코더/movie_project/src/routes/Home.js
+++ React/노마드 코더/movie_project/src/routes/Home.js
@@ -8,8 +8,8 @@ function Home() {
     const response = await fetch(
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
     );
-
     const json = await response.json();
+    console.log(json.data);
     setMovies(json.data.movies);
     setLoading(false);
   };
